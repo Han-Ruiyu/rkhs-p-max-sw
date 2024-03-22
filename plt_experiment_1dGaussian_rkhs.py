1c4169a6ce79d12ea19b7543de22ce6e00854859
Han-Ruiyu <151786198+Han-Ruiyu@users.noreply.github.com>
2024-03-22 12:18:18 -0400

    Add files via upload

---
+++ plt_experiment_1dGaussian_rkhs.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 19:15:36 2024
+
+@author: Han Ruiyu
+
+Experiments of the rkhs embedding part
+the rkhs has Gaussian kernel
+the sampled distribution can be 1d uniform distribution
+or 1d Gaussian
+"""
+
+from max_sliced_functions import *
+from time import time
+import matplotlib.pyplot as plt
+
+    
+number_of_points=[50,100,250, 500,1000]
+max_sw_distance=[0,0,0,0,0]
+
+
+for i in range(5):
+    start = time()
+    num=number_of_points[i]
+    
+    # number of times of monte-carlo
+    iterate=100
+    
+    max_sw_distance[i]=expected_max_swdistance_empirical_embed_into_gaussian_rkhs('Gaussian',2,num,iterate)
+    stop = time()
+    
+    print("Runtime for "+str(num) +" samples:"+ str((stop - start) / 60.0) + " min.\n")
+
+# if want to plot log-log
+#plt.loglog(number_of_points, max_sw_distance)
+#plt.title('log-log plot ${\mu}=\mathcal{N}(0,9)$')
+
+plt.plot(number_of_points, max_sw_distance)
+plt.title('${\mu}=v=\mathcal{N}(0,1), d_{test}=15$')
+plt.xlabel('number of sample points $n$',fontsize=18)
+plt.ylabel('$\mathbb{E}\overline{W}_2(\Phi_{\#}(\mu_n),\Phi_{\#}(\mu))$',fontsize=16)
+plt.grid()
+
+
+
+plt.show()
